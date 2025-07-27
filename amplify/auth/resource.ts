f40e52903fbd6be3b75dd889131c0b9009df344b
Apollo Rowe - WSL <1apollo.rowe@gmail.com>
2025-07-26 22:17:48 -0400

    added secrets and external providers

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from '@aws-amplify/backend';
 
 /**
  * Define and configure your auth resource
@@ -7,5 +7,27 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      logoutUrls: [
+        "http://localhost:5173/logout",
+        "https://www.budjit.net/logout"
+      ],
+      callbackUrls: [
+        "http://localhost:5173/profile",
+        "http://localhost:5173/plaid",
+        "https://www.budjit.net/profile",
+        "https://www.budjit.net/plaid",
+      ],
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET')
+      },
+      signInWithApple: {
+        clientId: secret('APPLE_CLIENT_ID'),
+        teamId: secret('APPLE_TEAM_ID'),
+        keyId: secret('APPLE_KEY_ID'),
+        privateKey: secret('APPLE_PRIVATE_KEY')
+      }
+    }
   },
 });
